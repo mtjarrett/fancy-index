86f7297f29e8fa757234a05e12b8d327e0a482bc
mtjarrett <michaeltjarrett@yahoo.com>
2018-09-11 14:35:47 -0700

    corrected file path

--- thumbnailer.py
+++ thumbnailer.py
@@ -1,12 +1,12 @@
 #/usr/bin/python
 import os
 #check and/or create hidden thumbnail directory
-if os.path.isdir("/var/www/html/sites/images/.thumbnails") == False:
-  os.system("mkdir /var/www/html/sites/images/.thumbnails")
+if os.path.isdir("/var/www/html/images/.thumbnails") == False:
+  os.system("mkdir /var/www/html/images/.thumbnails")
 #cycle through file and add thumbnails to thumbnail directory
-for filename in os.listdir("/var/www/html/sites/images"):
+for filename in os.listdir("/var/www/html/images"):
   if filename.endswith(".jpg"):
-    os.system("convert -thumbnail 256x256 /var/www/html/sites/images/%s /var/www/html/sites/images/.thumbnails/thumb.%s" % (filename,filename))
+    os.system("convert -thumbnail 256x256 /var/www/html/images/%s /var/www/html/images/.thumbnails/thumb.%s" % (filename,filename))
     continue
   else:
     continue
