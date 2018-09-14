4fb3a7d3c8e2e3cbbe246ee0d068b47739016f46
mtjarrett <michaeltjarrett@yahoo.com>
2018-09-14 08:42:55 -0700

    Add files via upload
    Requires file path as argv[1]

--- thumbnailer.py
+++ thumbnailer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import os
+import sys
 
 #rootDir = "/var/www/html/images"
-rootDir = sys.argv[0]
+rootDir = sys.argv[1]
+print("%s" % rootDir)
 
 for dirName, subdirList, fileList in os.walk(rootDir):
   subdirList[:] = [d for d in subdirList if not d[0] == '.'] 
