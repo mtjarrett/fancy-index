fa5618efde3a40dcba8190b97f36c76c92079f9a
mtjarrett <michaeltjarrett@yahoo.com>
2018-10-03 16:08:24 -0700

    Add files via upload
    Skips permission denials instead of dying

--- thumbnailer.py
+++ thumbnailer.py
@@ -1,48 +1,51 @@
 #!/usr/bin/python
 import os
 import sys
+import re
 
-#rootDir = "/var/www/html/images"
+#rootDir = "/var/www/html/image"
 rootDir = sys.argv[1]
 
+#Recursive walkthrough from "rootDir"
 for dirName, subdirList, fileList in os.walk(rootDir):
-  subdirList[:] = [d for d in subdirList if not d[0] == '.'] 
+  #bypass any directories that don't allow writing. Will still be notified in terminal
+  try:
+    #ignore hidden folders (starting with ".")
+    subdirList[:] = [d for d in subdirList if not d[0] == '.'] 
 
-  #check and/or create hidden thumbnail directory
-  if os.path.isdir("%s/.thumbnails" % (dirName)) == False:
-    os.system("mkdir %s/.thumbnails" % (dirName))
-    
-    
-  #cycle through file and add thumbnails to thumbnail directory
-  
-  for filename in os.listdir("%s" % (dirName)):
-    
-    #add image file extensions
-    if (filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".svg") or filename.endswith(".jpeg")  or filename.endswith(".gif") or filename.endswith(".tiff") or filename.endswith(".bmp")):
-      fileTime = float(os.path.getctime("%s/%s" % (dirName, filename)))
-      stripFile = os.path.splitext(os.path.basename(filename))[0]
-      if(os.path.isfile("%s/.thumbnails/thumb.%s.jpg" % (dirName, stripFile))):
-        thumbTime = float(os.path.getctime("%s/.thumbnails/thumb.%s.jpg" % (dirName, stripFile)))
-      else:
-        thumbTime = 0.0
-      if (fileTime > thumbTime):
-          os.system("convert -thumbnail 256x256 %s/%s %s/.thumbnails/thumb.%s.jpg" % (dirName, filename, dirName, stripFile))
+    #check and/or create hidden thumbnail directory
+    if os.path.isdir("%s/.thumbnails" % (dirName)) == False:
+      os.system("mkdir %s/.thumbnails" % (dirName))
+
+
+    #cycle through file and add thumbnails to thumbnail directory
+    for filename in os.listdir("%s" % (dirName)):
+      #filename = re.sub('*','\*', filename.rstrip())
+      #add image file extensions
+      if (filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".svg") or filename.endswith(".jpeg")  or filename.endswith(".gif") or filename.endswith(".tiff") or filename.endswith(".bmp")):
+        fileTime = float(os.path.getctime("%s/%s" % (dirName, filename)))
+        stripFile = os.path.splitext(os.path.basename(filename))[0]
+        if(os.path.isfile("%s/.thumbnails/thumb.%s.jpg" % (dirName, stripFile))):
+          thumbTime = float(os.path.getctime("%s/.thumbnails/thumb.%s.jpg" % (dirName, stripFile)))
+        else:
+          thumbTime = 0.0
+        if (fileTime > thumbTime):
+            os.system("convert -thumbnail 256x256 %s/\"%s\" %s/.thumbnails/thumb.\"%s\".jpg" % (dirName, filename, dirName, stripFile))
+            continue
+
+      #pdf thumbnailer      
+      elif (filename.endswith(".pdf")):
+        fileTime = float(os.path.getctime("%s/%s" % (dirName, filename)))
+        stripFile = os.path.splitext(os.path.basename(filename))[0]
+        if(os.path.isfile("%s/.thumbnails/thumb.%s.jpg" % (dirName, stripFile))):
+          thumbTime = float(os.path.getctime("%s/.thumbnails/thumb.%s.jpg" % (dirName, stripFile)))
+        else:
+          thumbTime = 0.0
+        if (fileTime > thumbTime):
+          os.system("convert -colorspace sRGB %s/\"%s\" -scale 256x256 -background white -flatten %s/.thumbnails/thumb.\"%s\".jpg" % (dirName, filename, dirName, stripFile))
           continue
-          
-    #pdf thumbnailer      
-    elif (filename.endswith(".pdf")):
-      fileTime = float(os.path.getctime("%s/%s" % (dirName, filename)))
-      stripFile = os.path.splitext(os.path.basename(filename))[0]
-      if(os.path.isfile("%s/.thumbnails/thumb.%s.jpg" % (dirName, stripFile))):
-        thumbTime = float(os.path.getctime("%s/.thumbnails/thumb.%s.jpg" % (dirName, stripFile)))
-      else:
-        thumbTime = 0.0
-      if (fileTime > thumbTime):
-        os.system("convert -colorspace sRGB %s/%s -scale 256x256 -background white -flatten %s/.thumbnails/thumb.%s.jpg" % (dirName, filename, dirName, stripFile))
-        continue
-            
-    else:
-      continue
 
-      
-      #print os.path.splitext(os.path.basename("hemanth.txt"))[0]
+      else:
+        continue
+  except OSError:
+    pass
